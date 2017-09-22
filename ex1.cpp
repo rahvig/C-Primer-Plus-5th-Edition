93311975e9a17acfcb1ab4c3df7f89b9e0c331c5
Rahul Vig <ravig51395@gmail.com>
2017-09-22 15:17:06 -0400

    Delete ex1.cpp

--- ex1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-
-int main(int argc, char* argv[]){
-  //C++ Program to display my name and address.
-  std::cout << "My name is Rahul Vig and I live in New York City" << std::endl;
-
-return 0;
-}
